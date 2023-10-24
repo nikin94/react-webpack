9a7a2a8faca4df8a73a93df98fd5d96dcc17c76b
Serhiy <nikin1994@gmail.com>
2023-10-24 03:02:31 +0100

    fix cors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import routes from './lib/routes'
 
-const router = createBrowserRouter(routes)
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+
+const router = createBrowserRouter(routes, {
+  basename: isDev ? '/' : '/react-webpack/'
+})
 
 const App = () => <RouterProvider router={router} />
 
--- src/lib/loaders.js
+++ src/lib/loaders.js
@@ -1,12 +1,14 @@
 import api from './api'
 
+const settings = { mode: 'cors' }
+
 export const albumsLoader = async ({ params }) =>
-  fetch(`${api.USERS}/${params.userId}/albums`)
+  fetch(`${api.USERS}/${params.userId}/albums`, settings)
 
 export const albumLoader = async ({ params }) =>
-  fetch(`${api.ALBUMS}/${params.albumId}/photos`)
+  fetch(`${api.ALBUMS}/${params.albumId}/photos`, settings)
 
 export const postsLoader = async ({ params }) =>
-  fetch(`${api.USERS}/${params.userId}/posts`)
+  fetch(`${api.USERS}/${params.userId}/posts`, settings)
 
-export const usersLoader = async () => fetch(api.USERS)
+export const usersLoader = async () => fetch(api.USERS, settings)
